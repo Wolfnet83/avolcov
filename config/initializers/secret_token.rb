ff922872f2504989f01d628fc060cabdec579b13
Andrei Volcov <andrey.volcov@gmail.com>
2013-11-29 15:01:06 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Avolcov::Application.config.secret_key_base = '88ece00086c361050a1a48d182adaac775da0bbb18f2e643abc342512fba4480b08e2a44cd50f7110de6d9d71795ff5b1eb32b7e756be9140aa67fec018430f5'
