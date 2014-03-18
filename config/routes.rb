971de1b6738d975dc37df438e619211a7218659a
Andrei Volcov <andrey.volcov@gmail.com>
2014-03-18 14:37:49 +0200

    First test commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Avolcov::Application.routes.draw do
-  get "about/index"
+  get "about/index", as: :about
+  get "projects/index", as: :projects
+  get "contacts/index", as: :contacts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
