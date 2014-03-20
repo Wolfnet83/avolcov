992a954e47364f3ab371abe992cb38f715e6c8a6
Andrei Volcov <andrey.volcov@gmail.com>
2014-03-20 16:57:08 +0200

    Failed commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    session[:locale] = params[:locale] if params[:locale].present?
+
+    logger.info "*"*80
+    logger.info session[:locale]
+    logger.info "*"*80
+
+    session[:locale] = I18n.default_locale
+    I18n.locale = session[:locale]
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def locale_switch  
+    I18n.locale == :ru ? 'en' : 'ru'
+  end
+
+
 end
